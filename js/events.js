b1b455fbaa9107f75ab9d9e7c46096deea4001d6
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-24 13:47:03 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,48 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on("keydown", function(key){
+    if(key.which == 71){
+      alert("G")
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
-function getIt (){
-  //The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
 
-  $("p").click(function(){
-  alert("Hey!");
-
-  });
-}
-
-function frameIt (){
-  //The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
-
-
-  $('img').on('load', function(){
-    $('img').addClass("tasty");
-  });
-
-
-
-}
-
-function pressIt (){
-
-  if (key.which === 71){
-    alert('G was pressed');
-  }
-});
-
-}
-
-function submitIt (){
-
-
-
-  $("form").on("submit", function() {
-
-    alert('Your form is going to be submitted now.');
-
-
-
-});
+getIt();
 
 });
